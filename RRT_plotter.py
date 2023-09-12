679c2ec7e7b5a9517048254fcb3a96f54182611b
Nader Ahmed <naderahmed2024@u.northwestern.edu>
2023-09-12 16:58:48 -0500

    plotter is working

--- RRT_plotter.py
+++ RRT_plotter.py
@@ -1,7 +1,8 @@
 
-from RRT import * 
+from RRT import RRT 
 import matplotlib.pyplot as plt
 from matplotlib.collections import LineCollection
+import numpy as np
 
 
 class Plotter:
@@ -11,9 +12,18 @@ class Plotter:
         self.fig,self.ax = plt.subplots()
         self.plot_vertices()
         self.plot_edges() 
+        self.plot_circle_obstacles()
+        self.ax.set_xlim(0,self.rrt.D[0])
+        self.ax.set_ylim(0,self.rrt.D[1])
+
+
+        ratio = 1.0
+        xleft, xright = self.ax.get_xlim()
+        ybottom, ytop = self.ax.get_ylim()
+        self.ax.set_aspect(abs((xright-xleft)/(ybottom-ytop))*ratio)
+
         plt.show()
 
-    
     def plot_vertices(self): 
         for v in self.rrt.G: 
             plt.plot(v[0],v[1],'bo')
@@ -30,10 +40,15 @@ class Plotter:
                                 colors='black', linestyle='solid')
         self.ax.add_collection(line_segments)
 
+    def plot_circle_obstacles(self): 
+        for crc in self.rrt.circle_obstacles:
+            circle = plt.Circle((crc[0],crc[1]),crc[2])
+            self.ax.add_patch(circle) 
 
+        
 def main(): 
 
-    rrt = RRT(q_init=(50,50),K=100,delta=1,D=(100,100))
+    rrt = RRT(q_init=(10,10),K=500,delta=1,D=(50,50))
     plotter = Plotter(rrt)
 
 main() 
